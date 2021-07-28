4dc83294635f62b6777b7eb90f1d41741cc66511
purvar <chenjiabin@purvar.com>
2021-07-28 10:26:13 +0800

    [update] .gitignore, data

--- sentiment/meta.py
+++ sentiment/meta.py
@@ -16,11 +16,11 @@ class GlobalParameters():
         self.embed_dim = 200
         self.hidden_dim = 30
         self.target_size = 3
-        self.dataSourceFilePath = "data/nCoV_100k_train.labled.utf.csv"
-        self.vocab_path = "data/vocab.dat"
-        self.model_path = "data/model.dat"
+        self.dataSourceFilePath = "data/sentiment/nCoV_100k_train.labled.utf.csv"
+        self.vocab_path = "data/sentiment/vocab.dat"
+        self.model_path = "data/sentiment/model.dat"
         self.criterion = torch.nn.CrossEntropyLoss()
-        jieba.load_userdict("data/jiebaDict.txt")
+        jieba.load_userdict("data/sentiment/jiebaDict.txt")
     def addVocab(self, vocab):
         self.vocab = vocab
         self.vocab_size = len(vocab)
@@ -46,7 +46,7 @@ def buildVocab(getIter):
 
 # 文本预处理, 将输入一段文本分词后清除异常字符, 返回词iter
 def pretreat(text):
-    with open("data/badWordList.txt") as f:
+    with open("data/sentiment/badWordList.txt") as f:
         badWordList = f.read().split('\n')
         # 匹配全角感叹号，问号，逗号，句号，汉字，数字
         pattern1 = re.compile("[\uff01\uff1f\uff0c\u3002\u4e00-\u9fff0-9]+")
